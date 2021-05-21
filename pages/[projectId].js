d70c45b97236e862206b60c4a5e3cf1981bd25e1
jeroenvanhattem <jeroenvanhattem@outlook.com>
2021-05-21 18:30:12 +0200

    Fixed issue for Vercel

---
+++ pages/[projectId].js
@@ -0,0 +1,11 @@
+import styles from '../styles/ProjectDetails.module.css'
+
+const ProjectDetails = (props) => {
+  return (
+    <div className={styles.projectDetails}>
+
+    </div>
+  )
+}
+
+export default ProjectDetails
