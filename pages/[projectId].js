02f31771cc4a219cfd434aa6e3fdef5d19ac39d4
jeroenvanhattem <jeroenvanhattem@outlook.com>
2021-05-21 18:34:33 +0200

    Fixed issue for Vercel

--- pages/[projectId].js
+++ pages/[projectId].js
@@ -1,11 +1,13 @@
+import ProjectDetail from '../Components/Projects/ProjectDetail'
 import styles from '../styles/ProjectDetails.module.css'
 
 const ProjectDetails = (props) => {
   return (
     <div className={styles.projectDetails}>
-
+      <ProjectDetail />
     </div>
   )
 }
+
 
 export default ProjectDetails
