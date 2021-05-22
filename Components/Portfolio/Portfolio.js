b3b058e77927adf196b73661e64d738f42b7102f
jeroenvanhattem <jeroenvanhattem@outlook.com>
2021-05-22 13:57:44 +0200

    Fixed Next.js error where portfolio wasn't loading?

--- Components/Portfolio/Portfolio.js
+++ Components/Portfolio/Portfolio.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import Project from './Project'
 import styles from './Portfolio.module.css'
 
-const Portfolio = () => {
+const Portfolio = (props) => {
   const [projects, setProjects] = useState(null)
   const BACKEND_URL = process.env.BACKEND_URL
 
@@ -21,15 +21,15 @@ const Portfolio = () => {
         {projects && projects.map((project) => {
           return (
             <div
-              key={project._id}
+              key={props.project._id}
               className={styles.project}>
               <Project
-                key={project._id}
-                className={styles.project}
-                id={project._id}
-                title={project.title}
-                description={project.description}
-                image={project.image}
+                key={props.project._id}
+                className={props.styles.project}
+                id={props.project._id}
+                title={props.project.title}
+                description={props.project.description}
+                image={props.project.image}
               />
             </div>
           )
