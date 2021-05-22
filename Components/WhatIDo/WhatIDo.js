f37766bce094c3252adf7042cf4430542f5c1977
jeroenvanhattem <jeroenvanhattem@outlook.com>
2021-05-22 17:02:54 +0200

    Added email to what i do page

--- Components/WhatIDo/WhatIDo.js
+++ Components/WhatIDo/WhatIDo.js
@@ -10,6 +10,7 @@ export default function Home() {
         <p>hey, i'm jeroen. i'm 22 years old. living near utrecht.</p>
         <p>i make music (hip-hop). i develop websites using react, getting better at it. i workout a lot, play football regularly.</p>
         <p>i'd love to move to new york</p>
+        <p>if you want to reach out to me. don't think twice and send me an email at jeroenvanhattem[at]outlook.com</p>
       </div>
 
       <div className={styles.skills}>
